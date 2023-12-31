26018d006f6feffe5c6aabc6f1457f1d62195ab8
Mamta sharma <your_email@example.com>
2023-12-30 19:14:22 -0800

    usestate hook with object

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from './component/header'
 import Sidenav from './component/Sidenav'
 import MainContent from './component/MainContent'
 import Footer from './component/Footer'
+import UseStateWithObject from './component/UseStateWithObject'
 
 
 function App() {
@@ -15,21 +16,23 @@ function App() {
     
    
    
-    <Container className="border border-primary">
-    <Row className='justify-content-md-center align-items-center  bg-success'  style={{ height: '200px' }}>
-      <Header title="HEADER"/>
-    </Row>
-    <Row  style={{ height: '400px' }}>
+//     <Container className="border border-primary">
+//     <Row className='justify-content-md-center align-items-center  bg-success'  style={{ height: '200px' }}>
+//       <Header title="HEADER"/>
+//     </Row>
+//     <Row  style={{ height: '400px' }}>
 
-<Col md={4} className='bg-primary d-flex justify-content-md-center align-items-center'><Sidenav title="SIDENAV" /></Col>
-<Col md={8}  className='bg-warning d-flex justify-content-md-center align-items-center'><MainContent title="MAIN CONTENT"/></Col>
+// <Col md={4} className='bg-primary d-flex justify-content-md-center align-items-center'><Sidenav title="SIDENAV" /></Col>
+// <Col md={8}  className='bg-warning d-flex justify-content-md-center align-items-center'><MainContent title="MAIN CONTENT"/></Col>
 
-    </Row>
-    <Row className='justify-content-md-center align-items-center bg-secondary '  style={{ height: '200px' }}>
-      <Footer title="FOOTER"/>
-    </Row>
-   </Container>
-
+//     </Row>
+//     <Row className='justify-content-md-center align-items-center bg-secondary '  style={{ height: '200px' }}>
+//       <Footer title="FOOTER"/>
+//     </Row>
+//    </Container>
+<>
+<UseStateWithObject></UseStateWithObject>
+</>
  
   )
 }
